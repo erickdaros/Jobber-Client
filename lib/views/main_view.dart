141ffe6a1709fb7be69ed98ff24a96ad35687cb1
Erick Daros <erick.silva@valedoacodigital.com.br>
2019-05-22 10:18:29 -0300

    Abstraido o Drawer da MainView para ser reutilizado

--- lib/views/main_view.dart
+++ lib/views/main_view.dart
@@ -6,6 +6,7 @@ import 'package:jobber/models/proposal_model.dart';
 import 'package:jobber/themes/jobber_theme.dart';
 import 'package:jobber/widgets/easing_animation.dart';
 import 'package:jobber/widgets/job_card.dart';
+import 'package:jobber/widgets/jobber_drawer.dart';
 
 class MainView extends StatefulWidget {
 
@@ -73,144 +74,7 @@ class _MainViewState extends State<MainView> {
         backgroundColor: JobberTheme.white,
         elevation: 0,
       ),
-      drawer: Drawer(
-        child: ListView(
-          padding: EdgeInsets.zero,
-          children: <Widget>[
-            UserAccountsDrawerHeader(
-              decoration: new BoxDecoration(
-                gradient: new LinearGradient(
-                  colors: [
-                    JobberTheme.purple,
-                    JobberTheme.purple.withOpacity(0.5),
-                  ],
-                  begin: const FractionalOffset(1.0, 0.0),
-                  end: const FractionalOffset(0.0, 0.0),
-                  stops: [0.0, 1.0],
-                  tileMode: TileMode.clamp
-                )
-              ),
-              currentAccountPicture: Container(
-                decoration: BoxDecoration(
-                    shape: BoxShape.circle,
-                    image: DecorationImage(
-                        fit: BoxFit.fill,
-                        image:AssetImage("assets/profile.jpg"))),
-              ),
-              accountName: new Container(
-                  child: Text(
-                    'Erick Daros Silva',
-                    style: TextStyle(color: Colors.white),
-                  )),
-              accountEmail: new Container(
-                  child: Text(
-                    'erickdarosilva@gmail.com',
-                    style: TextStyle(color: Colors.white),
-                  )),
-            ),
-            MaterialButton(
-              height: 1,
-              padding: EdgeInsets.all(20.0),
-              onPressed: () => {_onGridChange(!isGrid)},
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: <Widget>[
-                  Container(
-                      child: Row(
-                        children: <Widget>[
-                          Icon(Icons.view_column),
-                          Padding(
-                            padding: const EdgeInsets.only(left: 15),
-                            child: Text('Mostrar duas colunas'),
-                          ),
-                        ],
-                      )),
-                  Switch(
-                    value: isGrid,
-                    onChanged: _onGridChange,
-                    activeColor: JobberTheme.purple,
-                  ),
-                ],
-              ),
-            ),
-            ListTile(
-              title: Row(
-                children: <Widget>[
-                  Padding(
-                    padding: const EdgeInsets.only(right: 7),
-                    child: Icon(Icons.assessment),
-                  ),
-                  Text('Minhas Contribuições'),
-                ],
-              ),
-              onTap: () {
-                // Update the state of the app
-                // ...
-              },
-            ),
-            ListTile(
-              title: Row(
-                children: <Widget>[
-                  Padding(
-                    padding: const EdgeInsets.only(right: 7),
-                    child: Icon(Icons.add_to_photos),
-                  ),
-                  Text('Minhas Propostas'),
-                ],
-              ),
-              onTap: () {
-                // Update the state of the app
-                // ...
-              },
-            ),
-            ListTile(
-              title: Row(
-                children: <Widget>[
-                  Padding(
-                    padding: const EdgeInsets.only(right: 7),
-                    child: Icon(Icons.notifications),
-                  ),
-                  Text('Avisos'),
-                ],
-              ),
-              onTap: () {
-                // Update the state of the app
-                // ...
-              },
-            ),
-            ListTile(
-              title: Row(
-                children: <Widget>[
-                  Padding(
-                    padding: const EdgeInsets.only(right: 7),
-                    child: Icon(Icons.settings),
-                  ),
-                  Text('Configurações'),
-                ],
-              ),
-              onTap: () {
-                // Update the state of the app
-                // ...
-              },
-            ),
-            ListTile(
-              title: Row(
-                children: <Widget>[
-                  Padding(
-                    padding: const EdgeInsets.only(right: 7),
-                    child: Icon(Icons.exit_to_app),
-                  ),
-                  Text('Sair'),
-                ],
-              ),
-              onTap: () {
-                // Update the state of the app
-                // ...
-              },
-            ),
-          ],
-        ),
-      ),
+      drawer: JobberDrawer(),
       body: Container(
         key: viewPortKey,
         child: EasingAnimation(
